f5215ad11809fd11842417e82e1bb9ab05e9fe19
Zain Malik <zmalikshxil@gmail.com>
2017-12-05 15:20:10 +0100

    Add custom namespace support

--- restart.sh
+++ restart.sh
@@ -5,5 +5,5 @@ then
         echo "Pod name cannot be empty"
         exit 2
 else
-        kubectl get po "$1" -o yaml | kubectl replace --force -f -
+        kubectl get po "$1" -n "$KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE" -o yaml | kubectl replace --force -f -
 fi
